a0f9b605e9aa1dba0923363e729e0f247b41b21a
David Ostos <davidostos2@gmail.com.com>
2023-02-03 10:36:42 -0400

    agregando productos-controller y modificando la responsividad

---
+++ controllers/productos-controller.js
@@ -0,0 +1,42 @@
+import { productosServices } from "../js/producto/prducto-service.js";
+
+const nuevoProducto = (name,price,imageUrl)=>{
+
+    const card = document.createElement("div");
+
+    const contenido = `
+        <a href="#">
+            <img
+                src="${imageUrl}"
+                alt=""
+                class="coleccion__contenido__img"
+            />
+        </a>
+        <div class="coleccion__container__contenido">
+            <p class="coleccion__contenido__titulo">${name}</p>
+            <p class="coleccion__contenido__precio">$ ${price}</p>
+            <a href="producto.html">
+                <p class="coleccion__contenido__ver ver">Ver producto</p>
+            </a>
+        </div>
+    `;
+    card.innerHTML = contenido
+    card.classList.add('coleccion__tarjeta');
+    card.classList.add('container__column');
+    return card;
+}  
+
+const producto = document.querySelector('[data-producto-starWars]');
+const render = async () => {
+
+    try {
+        const listaProductos = await productosServices.listaProductos();
+        listaProductos.forEach(element => {
+            producto.appendChild(nuevoProducto(element.name,element.price,element.imageUrl))
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+render();
--- js/producto/prducto-service.js
+++ js/producto/prducto-service.js
@@ -1,6 +1,6 @@
 //GET
 
-const listaProductos = () => fetch('http://localhost:3000/producto').then(respuesta => respuesta.json());
+const listaProductos = () => fetch('https://bitter-rare-plaster.glitch.me/producto').then(respuesta => respuesta.json());
 
 export const productosServices = {
     listaProductos,
